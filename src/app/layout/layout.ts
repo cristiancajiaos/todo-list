5adef75a5deeb04adf3903d90d5be23638641c31
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-09 15:23:34 -0400

    Añadida implementación

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   standalone: false,
   templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  styleUrl: './layout.scss',
 })
-export class Layout {
+export class Layout implements DoCheck {
+  public value: number = 0;
+  public previousValue: number = 0;
+  public changesDetected: boolean = false;
 
+  constructor() {}
+
+  ngDoCheck(): void {
+    console.log('Layout component detected DoCheck lifecycle');
+    if (this.value !== this.previousValue) {
+      this.changesDetected = true;
+      this.previousValue = this.value;
+    }
+  }
+
+  public updateValue(): void {
+    this.value++;
+    setInterval(() => {
+      this.changesDetected = false;
+    }, 2000);
+  }
 }
